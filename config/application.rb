6308d3ed727612c2e0c3e09a06db2afafe3f76a7
watanabekouta-tech <ko_kirby_rui9898@icloud.com>
2020-08-21 17:21:24 -0700

    ログイン画面

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,11 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    end
+    config.i18n.default_locale = :ja
   end
 end
